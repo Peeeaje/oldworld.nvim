049f8f18363f2bea93e01588790e976e14fbc191
Jumpei Yamakawa <j.yamakawa@diamondhead.tech>
2024-06-05 12:20:59 +0900

    Make flash label as red

--- lua/oldworld/groups/integrations/flash.lua
+++ lua/oldworld/groups/integrations/flash.lua
@@ -1,7 +1,7 @@
 local p = require("oldworld.palette")
 
 return {
-    FlashLabel = { fg = p.bright_green, bg = p.bg, bold = true },
+    FlashLabel = { fg = p.bright_red, bg = p.bg, bold = true },
     FlashBackdrop = { fg = p.subtext4 },
     FlashMatch = { fg = p.bright_purple, bg = p.bg },
     FlashCurrent = { fg = p.bright_cyan, bg = p.bg },
