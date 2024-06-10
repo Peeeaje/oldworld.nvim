1bb2076b9993d843f6e7c480b7f37a9ed853bfd3
Jumpei Yamakawa <j.yamakawa@diamondhead.tech>
2024-06-10 15:55:22 +0900

    Add mini cursorword support

--- lua/oldworld/groups/integrations/mini.lua
+++ lua/oldworld/groups/integrations/mini.lua
@@ -2,4 +2,6 @@ local p = require("oldworld.palette")
 
 return {
     MiniIndentscopeSymbol = { fg = p.subtext4 },
+    MiniCursorword = { bg=p.bright_black },
+    MiniCursorwordCurrent = { bg=p.bright_black },
 }
