1d2cb08166c572318b76b83d6968d509063beb11
Jumpei Yamakawa <j.yamakawa@diamondhead.tech>
2024-06-11 09:38:14 +0900

    Add partial mini diff support

--- lua/oldworld/groups/integrations/mini.lua
+++ lua/oldworld/groups/integrations/mini.lua
@@ -2,4 +2,7 @@ local p = require("oldworld.palette")
 
 return {
     MiniIndentscopeSymbol = { fg = p.subtext4 },
+    MiniDiffSignAdd = { fg=p.green },
+    MiniDiffSignChange = { fg=p.yellow },
+    MiniDiffSignDelete = { fg=p.red },
 }
